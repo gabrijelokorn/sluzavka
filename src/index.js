ab65b15b34c1ebff99607e9de572a0ae3652cf4c
Peter-Ribic <pr0403@student.uni-lj.si>
2023-05-09 22:27:58 +0200

    merged

--- src/index.js
+++ src/index.js
@@ -67,6 +67,9 @@
 // // v data_ime si shrani objekte prebranih csv datatek z uporabo funkcije v razredu CSVmanager
 
 import _ from 'lodash';
+import postaja from './components/postaja';
+import povezava from './components/povezava';
+
 import data_lokacije from '../data/lokacije_postaj.csv';
 import data_SZ from '../data/povezave_SZ.csv'
 import data_physarum from '../data/povezave_physarum.csv'
@@ -81,20 +84,21 @@ function zgradi_postaje (seznam_postaj) {
     return postaje;
 }
 const postaje = zgradi_postaje(data_lokacije);
-console.log(postaje);
 
-// function zgradi_omrezje (seznam_povezav) {
-//     let omrezje = [];
-//     seznam_povezav.forEach(element => {
-//         omrezje.push(new povezava(postaje.find(p => p.ime == element.Ime_postaje_1), postaje.find(p => p.ime == element.Ime_postaje_2)));
-//     });
+function zgradi_omrezje (seznam_povezav) {
+    let omrezje = [];
+    seznam_povezav.forEach(element => {
+        omrezje.push(new povezava(postaje.find(p => p.ime == element.Ime_postaje_1), postaje.find(p => p.ime == element.Ime_postaje_2)));
+    });
     
-//     return omrezje;
-// }
-// const omrezje_SZ = zgradi_omrezje(data_SZ);
-// const omrezje_physarum = zgradi_omrezje(data_physarum);
-// const omrezje_computer = zgradi_omrezje(data_computer);
+    return omrezje;
+}
 
+const omrezje_SZ = zgradi_omrezje(data_SZ);
+const omrezje_physarum = zgradi_omrezje(data_physarum);
+const omrezje_computer = zgradi_omrezje(data_computer);
+
+console.log(omrezje_SZ);
 
 // function dolzina_omrezja(omrezje){
 //     let dolzina = 0;
