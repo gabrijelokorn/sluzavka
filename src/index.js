7de640c681e8ba45d494bb5d1aed9378f818ab59
Peter-Ribic <pr0403@student.uni-lj.si>
2023-05-09 22:43:40 +0200

    refactored

--- src/index.js
+++ src/index.js
@@ -1,71 +1,3 @@
-// // const csvManager = require("./utils/csvManager.js");
-// import {csvManager} from './utils/csvManager';
-// // const postaja = require("./components/postaja");
-// import {postaja} from './components/postaja'
-// // const povezava = require("./components/povezava");
-// import {povezava} from './components/povezava'
-// // TODO: Sestavi računalniško generirano omrežje z uporabo postaj in zapiši v povezave_computer.csv
-
-// // Pripravi branje vsake cdv datoteke
-// const data_directory        = "../data/";
-
-// const lokacije_csv          = data_directory + "lokacije_postaj.csv";
-// const locReader             = new csvManager(lokacije_csv);
-
-// const povezave_SZ_csv       = data_directory + "povezave_SZ.csv";
-// const SZReader              = new csvManager(povezave_SZ_csv);
-
-// const povezave_physarum_csv = data_directory + "povezave_physarum.csv";
-// const physarumReader        = new csvManager(povezave_physarum_csv);
-
-// const povezave_computer_csv = data_directory + "povezave_computer.csv";
-// const computerReader        = new csvManager(povezave_computer_csv);
-
-
-// // v data_ime si shrani objekte prebranih csv datatek z uporabo funkcije v razredu CSVmanager
-// const data_lokacije         = locReader.readCSV();
-// const data_SZ               = SZReader.readCSV();
-// const data_physarum         = physarumReader.readCSV();
-// const data_computer         = computerReader.readCSV();
-
-// // z uporabo razredov Postaja in Povezava sestavi seznam postaj in povezav za vsa tri omrežja
-// function zgradi_postaje (seznam_postaj) {
-//     let postaje = [];
-//     seznam_postaj.forEach(element => {
-//         postaje.push(new postaja(element.Ime_postaje, element.x, element.y));
-//     });
-//     return postaje;
-// }
-// const postaje = zgradi_postaje(data_lokacije);
-
-// function zgradi_omrezje (seznam_povezav) {
-//     let omrezje = [];
-//     seznam_povezav.forEach(element => {
-//         omrezje.push(new povezava(postaje.find(p => p.ime == element.Ime_postaje_1), postaje.find(p => p.ime == element.Ime_postaje_2)));
-//     });
-    
-//     return omrezje;
-// }
-// const omrezje_SZ = zgradi_omrezje(data_SZ);
-// const omrezje_physarum = zgradi_omrezje(data_physarum);
-// const omrezje_computer = zgradi_omrezje(data_computer);
-
-
-// function dolzina_omrezja(omrezje){
-//     let dolzina = 0;
-//     omrezje.forEach(element => {
-//         dolzina += element.razdalja();
-//     }); 
-//     return dolzina;
-// }
-
-// // TODO: kliči floyd-warshall za vsa tri omrežja in dobi ven seznam vseh najkrajših poti in postaj na teh poteh
-
-// // TODO: analiza podatkov
-// // TODO: funkcije za izris v html datoteki
-
-// // v data_ime si shrani objekte prebranih csv datatek z uporabo funkcije v razredu CSVmanager
-
 import _ from 'lodash';
 import postaja from './components/postaja';
 import povezava from './components/povezava';
@@ -87,6 +19,7 @@ const postaje = zgradi_postaje(data_lokacije);
 
 function zgradi_omrezje (seznam_povezav) {
     let omrezje = [];
+
     seznam_povezav.forEach(element => {
         omrezje.push(new povezava(postaje.find(p => p.ime == element.Ime_postaje_1), postaje.find(p => p.ime == element.Ime_postaje_2)));
     });
@@ -98,15 +31,21 @@ const omrezje_SZ = zgradi_omrezje(data_SZ);
 const omrezje_physarum = zgradi_omrezje(data_physarum);
 const omrezje_computer = zgradi_omrezje(data_computer);
 
-console.log(omrezje_SZ);
 
-// function dolzina_omrezja(omrezje){
-//     let dolzina = 0;
-//     omrezje.forEach(element => {
-//         dolzina += element.razdalja();
-//     }); 
-//     return dolzina;
-// }
+/* izpiši množice postaj za omrežje */
+// console.log(omrezje_SZ);
+// console.log(omrezje_physarum);
+// console.log(omrezje_computer);
+
+function dolzina_omrezja(omrezje){
+    let dolzina = 0;
+    omrezje.forEach(element => {
+        dolzina += element.razdalja();
+    }); 
+    return dolzina;
+}
+
+console.log(dolzina_omrezja(omrezje_SZ));
 
 // // TODO: kliči floyd-warshall za vsa tri omrežja in dobi ven seznam vseh najkrajših poti in postaj na teh poteh
 
